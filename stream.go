73f09602cf80d17f7453a8e39ebb8a37e1e97eca
Christoph Oelmüller <christoph.oelmueller@justwatch.com>
2017-09-19 11:54:54 +0200

    add service name label to stream watcher metrics

--- stream.go
+++ stream.go
@@ -15,15 +15,11 @@ var (
 	events = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "kuberesolver_watch_events",
 		Help: "number of watch events partitioned by type",
-	}, []string{"event_type"})
+	}, []string{"service", "event_type"})
 )
 
 func init() {
 	prometheus.MustRegister(events)
-	events.WithLabelValues("added")
-	events.WithLabelValues("modified")
-	events.WithLabelValues("deleted")
-	events.WithLabelValues("error")
 }
 
 // Interface can be implemented by anything that knows how to watch and report changes.
@@ -115,7 +111,7 @@ func (sw *streamWatcher) Decode() (Event, error) {
 	}
 	switch got.Type {
 	case Added, Modified, Deleted, Error:
-		events.WithLabelValues(strings.ToLower(string(got.Type))).Inc()
+		events.WithLabelValues(got.Object.Metadata.Name, strings.ToLower(string(got.Type))).Inc()
 		return got, nil
 	default:
 		return Event{}, fmt.Errorf("got invalid watch event type: %v", got.Type)
